904e225781f1ef7a557929a6ee9ff778da2665f2
Raya Taufik <rayataufik4@gmail.com>
2023-12-23 20:18:21 +0700

    secure_asset

--- resources/views/pages/app.blade.php
+++ resources/views/pages/app.blade.php
@@ -6,9 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tokotani</title>
 
-    <link rel="icon" type="image/png" href="{{ asset('assets/images/Favicon.png') }}">
+    <link rel="icon" type="image/png" href="{{ secure_asset('assets/images/Favicon.png') }}">
     <!-- CSS Start -->
-    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('assets/css/style.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
