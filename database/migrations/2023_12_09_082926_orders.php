a2c91467e592b210f7d4194d277139bf8fec6d78
Raya Taufik <rayataufik4@gmail.com>
2023-12-23 20:52:33 +0700

    fix db

--- database/migrations/2023_12_09_082926_orders.php
+++ database/migrations/2023_12_09_082926_orders.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('order', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('transaction_id');
             $table->unsignedBigInteger('product_id');
