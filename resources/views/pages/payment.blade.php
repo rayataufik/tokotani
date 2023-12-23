404102fefa7c68e8846509164e9360e3318d103c
Raya Taufik <rayataufik4@gmail.com>
2023-12-23 22:01:19 +0700

    menambahkan tutor pembayran

--- resources/views/pages/payment.blade.php
+++ resources/views/pages/payment.blade.php
@@ -14,16 +14,19 @@
                         <div class="col-6">Total Pembayaran</div>
                         <div class="col-6 text-end">Rp {{ number_format($transaction->total_tagihan, 0, ',') }}
                         </div>
-                        <div class="col-6">Expiry Time</div>
+                        <div class="col-6">Expired Time</div>
                         <div class="col-6 text-end">{{ $transaction->expiry_time }}</div>
                         <div class="col-6">Bank</div>
                         <div class="col-6 text-end">{{ $transaction->va_number }}</div>
                     </div>
-                    <p>Cara Melakukan Pembayaran</p>
-                    <p>Kunjungi <a
-                            href="https://simulator.sandbox.midtrans.com">https://simulator.sandbox.midtrans.com</a></p>
-                    <p>pilih Virtual Account</p>
-                    <p>pilih Bank Yang Anda Gunakan</p>
+                    <p class="mt-3">Cara Melakukan Pembayaran</p>
+                    <ul>
+                        <li>Kunjungi <a
+                                href="https://simulator.sandbox.midtrans.com">https://simulator.sandbox.midtrans.com</a>
+                        </li>
+                        <li>pilih Virtual Account</li>
+                        <li>pilih Bank Yang Anda Gunakan</li>
+                    </ul>
                     <div class="d-grid gap-2 mt-2">
                         <a href="/order-list" class="btn btn-outline-login">Sudah Bayar</a>
                     </div>
